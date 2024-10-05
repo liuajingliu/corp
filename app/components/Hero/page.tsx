e5c88ceddbb051a422e0e734c51f5bd5354532bd
liujingliu <liujingliu@163.com>
2024-10-05 16:53:54 +0800

    fix ug

--- app/components/Hero/page.tsx
+++ app/components/Hero/page.tsx
@@ -6,18 +6,22 @@ interface HeroProps {
   text: string;
 }
 
-export default function Hero(props: HeroProps) {
-  const { imageSrc, altTxt, text } = props;
-
+export default function Hero({ imageSrc, altTxt, text }: HeroProps) {
   return (
-     <div className="h-screen relative">
+    <div className="h-screen relative">
       <div className="absolute -z-10 inset-0">
-        <Image src={imageSrc} alt={altTxt} className="" fill style={{ objectFit: "cover" }}/>
+        <Image
+          src={imageSrc}
+          alt={altTxt}
+          className=""
+          fill
+          style={{ objectFit: "cover" }}
+        />
         <div className="absolute inset-0 bg-gradient-to-r from-gray-700" />
       </div>
       <div className="flex justify-center mx-auto pt-48">
         <div className="font-bold text-6xl text-white">{text}</div>
       </div>
     </div>
-  )
+  );
 }
