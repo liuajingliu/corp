839dd23f50f62e23bec4daf1f6d9c24929034409
liujingliu <liujingliu@163.com>
2024-10-05 16:40:38 +0800

    delete uselsee code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Inter, Comforter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = localFont({
--- app/test/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function notFound() {
-  return (
-    <div>
-      我是自定义的局部的 notFound
-    </div>
-  )
-}
